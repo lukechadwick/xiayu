8ba5535992bbf3c80d6c8a7b968a6c50fc9f92fc
Author Name <llukechadwick@gmail.com>
2018-12-04 16:20:46 +1300

    remove players from array once dead to improve loop performance

--- src/gameJS/collision.js
+++ src/gameJS/collision.js
@@ -1,36 +1,43 @@
-import {
-	playerArray,
-	bulletArray,
-	platformArray,
-	playerNumber,
-	boss
-} from "../index";
+import { playerArray, bulletArray, platformArray, boss } from '../index';
 
 export function hitDetection() {
 	for (let i = 0; i < bulletArray.bullets.length; i++) {
 		for (let j = 0; j < playerArray.players.length; j++) {
-			if (bulletArray.bullets[i].x - playerArray.players[j].x < 20 && bulletArray.bullets[i].x - playerArray.players[j].x > 0 &&
-				bulletArray.bullets[i].y - playerArray.players[j].y < 40 && bulletArray.bullets[i].y - playerArray.players[j].y > 0) {
+			if (
+				bulletArray.bullets[i].x - playerArray.players[j].x < 20 &&
+				bulletArray.bullets[i].x - playerArray.players[j].x > 0 &&
+				bulletArray.bullets[i].y - playerArray.players[j].y < 40 &&
+				bulletArray.bullets[i].y - playerArray.players[j].y > 0
+			) {
 				playerArray.players[[j]].health -= 10;
 				bulletArray.bullets[[i]].y = bulletArray.bullets[[i]].y + 1000;
-				console.log('Player ' + [j] + " Hit, Current Health: " + playerArray.players[[j]].health)
+				// console.log(
+				// 	'Player ' +
+				// 		[j] +
+				// 		' Hit, Current Health: ' +
+				// 		playerArray.players[[j]].health
+				// );
 			}
 		}
 	}
 }
 
 export function isOnPlatform() {
-	for (let i = 0; i < playerNumber; i++) {
+	for (let i = 0; i < playerArray.players.length; i++) {
 		for (let j = 0; j < platformArray.plat.length; j++) {
-			if (playerArray.players[i].y > platformArray.plat[j].y - playerArray.players[i].height &&
+			if (
+				playerArray.players[i].y >
+					platformArray.plat[j].y - playerArray.players[i].height &&
 				playerArray.players[i].y < platformArray.plat[j].y &&
 				playerArray.players[i].x > platformArray.plat[j].x - 20 &&
-				playerArray.players[i].x < platformArray.plat[j].endX) {
+				playerArray.players[i].x < platformArray.plat[j].endX
+			) {
 				playerArray.players[i].jumping = false;
 
 				if (playerArray.players[i].health > 1) {
 					playerArray.players[i].yVelocity = 0;
-					playerArray.players[i].y = platformArray.plat[j].y - playerArray.players[i].height + 4;
+					playerArray.players[i].y =
+						platformArray.plat[j].y - playerArray.players[i].height + 4;
 				}
 			}
 		}
@@ -40,12 +47,15 @@ export function isOnPlatform() {
 export function bulletCollisionPlat() {
 	for (let i = 0; i < bulletArray.bullets.length; i++) {
 		for (let j = 0; j < platformArray.plat.length; j++) {
-			if (bulletArray.bullets[i].x - platformArray.plat[j].x < 50 && bulletArray.bullets[i].x - platformArray.plat[j].x > 0 &&
-				bulletArray.bullets[i].y - platformArray.plat[j].y < 10 && bulletArray.bullets[i].y - platformArray.plat[j].y > 0) {
+			if (
+				bulletArray.bullets[i].x - platformArray.plat[j].x < 50 &&
+				bulletArray.bullets[i].x - platformArray.plat[j].x > 0 &&
+				bulletArray.bullets[i].y - platformArray.plat[j].y < 10 &&
+				bulletArray.bullets[i].y - platformArray.plat[j].y > 0
+			) {
 				if (bulletArray.bullets[i].hasBounced != 1) {
-					bulletArray.bullets[[i]].dy = -bulletArray.bullets[[i]].dy
-                    bulletArray.bullets[i].hasBounced = 1
-
+					bulletArray.bullets[[i]].dy = -bulletArray.bullets[[i]].dy;
+					bulletArray.bullets[i].hasBounced = 1;
 				}
 			}
 		}
@@ -55,12 +65,15 @@ export function bulletCollisionPlat() {
 export function bulletCollision() {
 	for (let i = 0; i < bulletArray.bullets.length; i++) {
 		for (let j = 0; j < bulletArray.bullets.length; j++) {
-			if (bulletArray.bullets[i].x - bulletArray.bullets[j].x < 5 && bulletArray.bullets[i].x - bulletArray.bullets[j].x > -5 &&
-				bulletArray.bullets[i].y - bulletArray.bullets[j].y < 5 && bulletArray.bullets[i].y - bulletArray.bullets[j].y > -5) {
+			if (
+				bulletArray.bullets[i].x - bulletArray.bullets[j].x < 5 &&
+				bulletArray.bullets[i].x - bulletArray.bullets[j].x > -5 &&
+				bulletArray.bullets[i].y - bulletArray.bullets[j].y < 5 &&
+				bulletArray.bullets[i].y - bulletArray.bullets[j].y > -5
+			) {
 				if (i != j) {
-					bulletArray.bullets[[i]].dy = -5 * Math.random()
-					bulletArray.bullets[[j]].dy = 5 * Math.random()
-
+					bulletArray.bullets[[i]].dy = -5 * Math.random();
+					bulletArray.bullets[[j]].dy = 5 * Math.random();
 				}
 			}
 		}
@@ -70,14 +83,17 @@ export function bulletCollision() {
 export function bossHit() {
 	for (let i = 0; i < bulletArray.bullets.length; i++) {
 		for (let j = 0; j < 1; j++) {
-			if (bulletArray.bullets[i].x - boss.x < 80 && bulletArray.bullets[i].x - boss.x > 50 &&
-				bulletArray.bullets[i].y - boss.y < 120 && bulletArray.bullets[i].y - boss.y > 0) {
+			if (
+				bulletArray.bullets[i].x - boss.x < 80 &&
+				bulletArray.bullets[i].x - boss.x > 50 &&
+				bulletArray.bullets[i].y - boss.y < 120 &&
+				bulletArray.bullets[i].y - boss.y > 0
+			) {
 				if (boss.ammo == 0 && bulletArray.bullets[i].origin != 1) {
 					boss.health -= 10;
 					bulletArray.bullets[i].y = bulletArray.bullets[i].y + 1000;
-					console.log('Boss ' + ' Hit, Current Health: ' + boss.health)
+					// console.log('Boss ' + ' Hit, Current Health: ' + boss.health);
 				}
-
 			}
 		}
 	}
--- src/gameJS/physics.js
+++ src/gameJS/physics.js
@@ -1,64 +1,68 @@
 import {
-    playerArray,
-    playerNumber,
-    windowHeight,
-    windowWidth,
-    groundHeight,
-    playerSize,
-    boss,
-} from "../index";
+	playerArray,
+	windowHeight,
+	windowWidth,
+	groundHeight,
+	playerSize,
+	boss
+} from '../index';
 
 export function physics() {
-    for (let i = 0; i < playerNumber; i++) {
+	for (let i = 0; i < playerArray.players.length; i++) {
+		//Player Movement
+		if (
+			playerArray.players[i].upState == true &&
+			playerArray.players[i].jumping == false
+		) {
+			playerArray.players[i].yVelocity -= 20;
+			playerArray.players[i].jumping = true;
+		}
+		if (playerArray.players[i].leftState) {
+			playerArray.players[i].facingLeft = true;
+			playerArray.players[i].xVelocity -= 0.4;
+		}
+		if (playerArray.players[i].rightState) {
+			playerArray.players[i].facingLeft = false;
+			playerArray.players[i].xVelocity += 0.4;
+		}
 
-        //Player Movement
-        if (playerArray.players[i].upState == true && playerArray.players[i].jumping == false) {
-            playerArray.players[i].yVelocity -= 20;
-            playerArray.players[i].jumping = true;
-        }
-        if (playerArray.players[i].leftState) {
-            playerArray.players[i].facingLeft = true;
-            playerArray.players[i].xVelocity -= 0.4;
-        }
-        if (playerArray.players[i].rightState) {
-            playerArray.players[i].facingLeft = false;
-            playerArray.players[i].xVelocity += 0.4;
-        }
+		// gravity
+		playerArray.players[i].yVelocity += 1.2;
 
-        // gravity
-        playerArray.players[i].yVelocity += 1.2;
-        
-        playerArray.players[i].x += playerArray.players[i].xVelocity;
-        playerArray.players[i].y += playerArray.players[i].yVelocity;
+		playerArray.players[i].x += playerArray.players[i].xVelocity;
+		playerArray.players[i].y += playerArray.players[i].yVelocity;
 
-         if (boss.health < 1)
-             boss.y+= 0.5
+		if (boss.health < 1) boss.y += 0.5;
 
-        // friction
-        playerArray.players[i].xVelocity *= 0.9;
-        playerArray.players[i].yVelocity *= 0.9;
+		// friction
+		playerArray.players[i].xVelocity *= 0.9;
+		playerArray.players[i].yVelocity *= 0.9;
 
-        //If player is dead take away collision with floor
-        if (playerArray.players[i].health > 1) {
-            // if rectangle is falling below floor line
-            if (playerArray.players[i].y > windowHeight - groundHeight - playerArray.players[i].height) {
-                playerArray.players[i].y = windowHeight - groundHeight - playerArray.players[i].height;
-                playerArray.players[i].jumping = false;
-                playerArray.players[i].yVelocity = 0;
-            }
-        }
-    }
+		//If player is dead take away collision with floor
+		if (playerArray.players[i].health > 1) {
+			// if rectangle is falling below floor line
+			if (
+				playerArray.players[i].y >
+				windowHeight - groundHeight - playerArray.players[i].height
+			) {
+				playerArray.players[i].y =
+					windowHeight - groundHeight - playerArray.players[i].height;
+				playerArray.players[i].jumping = false;
+				playerArray.players[i].yVelocity = 0;
+			}
+		}
+	}
 }
 
 export function boundaries() {
-    for (let i = 0; i < playerNumber; i++) {
-        // if player is going off the left of the screen
-        if (playerArray.players[i].x < -playerSize) {
-            playerArray.players[i].x = -10;
-            playerArray.players[i].xVelocity = 0;
-        }
-        // if player goes past right boundary
-        if (playerArray.players[i].x > windowWidth)
-            playerArray.players[i].x = windowWidth;
-    }
+	for (let i = 0; i < playerArray.players; i++) {
+		// if player is going off the left of the screen
+		if (playerArray.players[i].x < -playerSize) {
+			playerArray.players[i].x = -10;
+			playerArray.players[i].xVelocity = 0;
+		}
+		// if player goes past right boundary
+		if (playerArray.players[i].x > windowWidth)
+			playerArray.players[i].x = windowWidth;
+	}
 }

9aad8d5b90d8bb4f5a321411e1c185925ed7631a
Author Name <llukechadwick@gmail.com>
2018-12-04 18:14:58 +1300

    fix issue of players escaping boundaries

--- src/gameJS/boss.js
+++ src/gameJS/boss.js
@@ -1,53 +1,43 @@
-import { 
-    boss, 
-    gameWindow, 
-    windowWidth,
-    windowHeight
-} from "../index";
+import { boss, gameWindow, windowWidth, windowHeight } from '../index';
 
-import {
-    createBullet
-} from "./projectiles"
+import { createBullet } from './projectiles';
 
-export function bossBehavior () {
-    moveBoss();
+export function bossBehavior() {
+	moveBoss();
 
-    wallCollisionDetection();
+	wallCollisionDetection();
 
-    bossAttack();
+	bossAttack();
 }
 
 function moveBoss() {
-    boss.x += boss.dx;
-    boss.y += boss.dy;
+	boss.x += boss.dx;
+	boss.y += boss.dy;
 }
 
-function bossAttack(){
-    if (boss.ammo > 0) {
-        boss.ammo--
-        createBullet('b');
-    }
+function bossAttack() {
+	if (boss.ammo > 0) {
+		boss.ammo--;
+		createBullet('b');
+	}
 }
 
 function wallCollisionDetection() {
-    if (boss.x + boss.dx > windowWidth + 10 || boss.x + boss.dx < -130) {
-        console.log('Boss: Changing X direction')
-        boss.dx = -boss.dx;
-    }
-    if (boss.y + boss.dy > windowHeight - 100 || boss.y + boss.dy < -50) {
-        boss.dy = -boss.dy;
-        console.log('Boss: Changing Y direction')
-    }
+	if (boss.x + boss.dx > windowWidth + 10 || boss.x + boss.dx < -130) {
+		console.log('Boss: Changing X direction');
+		boss.dx = -boss.dx;
+	}
+	if (boss.y + boss.dy > windowHeight - 100 || boss.y + boss.dy < -50) {
+		boss.dy = -boss.dy;
+		console.log('Boss: Changing Y direction');
+	}
 }
 
 export function drawBoss() {
-    let drawing = new Image();
-    
-        if (boss.ammo == 0)
-        drawing.src = "./assets/boss.png";
-        else
-        drawing.src = "./assets/bossGreen.png";
-    
+	let drawing = new Image();
 
-    gameWindow.drawImage(drawing, boss.x,  boss.y);
+	if (boss.ammo == 0) drawing.src = './assets/boss.png';
+	else drawing.src = './assets/bossGreen.png';
+
+	gameWindow.drawImage(drawing, boss.x, boss.y);
 }

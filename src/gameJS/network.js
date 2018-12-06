e60e3983cd5a0f5009fe08f3b2052b80b25af640
lukechadwick <llukechadwick@gmail.com>
2018-12-07 12:51:57 +1300

    bullets and platforms synced

--- src/gameJS/network.js
+++ src/gameJS/network.js
@@ -1,19 +1,36 @@
-import { playerArray, bulletArray, boss } from '../index';
+import { playerArray, bulletArray, boss, platformArray } from '../index';
 
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000');
 
 let client = 0;
 
+document.addEventListener('DOMContentLoaded', createEventListeners);
+
+function createEventListeners() {
+  //Retrieve amount of real players from game setup window
+  document.getElementById('rp0').onclick = function() {
+    client = 1;
+  };
+  document.getElementById('rp1').onclick = function() {
+    client = 0;
+  };
+}
+
 socket.on('playerSync', function(msg) {
   // console.log(msg);
   if (client == 1) playerArray.players = msg;
 });
 
-// socket.on('projectileSync', function(msg) {
-//   // console.log(msg);
-//   bulletArray.bullets = msg;
-// });
+socket.on('projectileSync', function(msg) {
+  // console.log(msg);
+  if (client == 1) bulletArray.bullets = msg;
+});
+
+socket.on('platformSync', function(msg) {
+  // console.log(msg);
+  if (client == 1) platformArray.plat = msg;
+});
 
 // socket.on('playerSync', function(msg) {
 //   // console.log(msg);
@@ -34,7 +51,9 @@ export function send() {
   // console.log('Sending Packet:');
   if (client == 0) {
     socket.emit('playerSync', playerArray.players);
-    socket.emit('projectileSync', bulletArray);
+    socket.emit('projectileSync', bulletArray.bullets);
+    socket.emit('platformSync', platformArray.plat);
+
     socket.emit('bossSync', boss);
   }
 }

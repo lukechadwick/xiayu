6d5596fe552af5fc9b09250379be92745112f1d9
lukechadwick <llukechadwick@gmail.com>
2018-12-07 15:41:05 +1300

    let client change IP address

--- server/index.js
+++ server/index.js
@@ -13,34 +13,24 @@ io = socket(app);
 //Log when user connects/disconnects
 io.on('connection', function(socket) {
   console.log('User connected from:', socket.handshake.headers.host);
-  socket.on('disconnect', function() {});
+  socket.on('disconnect', function() {
+    socket.disconnect();
+  });
 });
 
 io.on('connection', function(socket) {
   socket.on('playerSync', function(msg) {
     io.emit('playerSync', msg);
   });
-});
-
-io.on('connection', function(socket) {
   socket.on('projectileSync', function(msg) {
     io.emit('projectileSync', msg);
   });
-});
-
-io.on('connection', function(socket) {
   socket.on('bossSync', function(msg) {
     io.emit('bossSync', msg);
   });
-});
-
-io.on('connection', function(socket) {
   socket.on('platformSync', function(msg) {
     io.emit('platformSync', msg);
   });
-});
-
-io.on('connection', function(socket) {
   socket.on('gameSync', function(msg) {
     io.emit('gameSync', msg);
   });

f44e5846d8e16460fe8309f40c1fa58c34120535
luke <llukechadwick@gmail.com>
2019-02-01 15:13:12 +1300

    network control sync working, though need to fix networked shooting control

--- src/gameJS/network.js
+++ src/gameJS/network.js
@@ -1,50 +1,60 @@
-import { playerArray, bulletArray, boss, platformArray } from '../index';
-import { setTime, countDown, startGameTime } from './boss';
+import { playerArray, bulletArray, boss, platformArray } from "../index";
+import { setTime, countDown, startGameTime } from "./boss";
 
 //init socket
-const io = require('socket.io-client');
-let socket = io.connect('http://' + document.getElementById('ipAddress').value);
+const io = require("socket.io-client");
+let socket = io.connect("http://" + document.getElementById("ipAddress").value);
 
 export let client = 2;
 
-document.addEventListener('DOMContentLoaded', createEventListeners);
+document.addEventListener("DOMContentLoaded", createEventListeners);
 function createEventListeners() {
   //Change IP address
-  document.getElementById('connectToGame').onclick = () => {
-    socket.io.uri = 'http://' + document.getElementById('ipAddress').value;
+  document.getElementById("connectToGame").onclick = () => {
+    socket.io.uri = "http://" + document.getElementById("ipAddress").value;
   };
 
   //Set client/server flag
-  document.getElementById('client').onclick = () => {
+  document.getElementById("client").onclick = () => {
     client = 1;
   };
-  document.getElementById('server').onclick = () => {
+  document.getElementById("server").onclick = () => {
     client = 0;
   };
 }
 
 //Listeners for client
-socket.on('playerSync', msg => {
+socket.on("playerSync", msg => {
   if (client == 1) playerArray.players = msg;
 });
-socket.on('projectileSync', msg => {
+socket.on("projectileSync", msg => {
   if (client == 1) bulletArray.bullets = msg;
 });
-socket.on('platformSync', msg => {
+socket.on("platformSync", msg => {
   if (client == 1) platformArray.plat = msg;
 });
 
-socket.on('controlSync', msg => {
+socket.on("controlSync", msg => {
   if (client == 0) {
-    if (msg == 'up') {
-      playerArray.players[1].upState = true;
-    } else {
-      playerArray.players[1].upState = false;
+    if (msg.control == "up") {
+      playerArray.players[1].upState = msg.state;
+    }
+    if (msg.control == "duck") {
+      playerArray.players[1].duckState = msg.state;
+    }
+    if (msg.control == "left") {
+      playerArray.players[1].leftState = msg.state;
+    }
+    if (msg.control == "right") {
+      playerArray.players[1].rightState = msg.state;
+    }
+    if (msg.control == "shoot") {
+      playerArray.players[1].shootState = msg.state;
     }
   }
 });
 
-socket.on('bossSync', msg => {
+socket.on("bossSync", msg => {
   if (client == 1) {
     boss.x = msg.x;
     boss.y = msg.y;
@@ -55,7 +65,7 @@ socket.on('bossSync', msg => {
     boss.state = msg.state;
   }
 });
-socket.on('gameSync', msg => {
+socket.on("gameSync", msg => {
   if (client == 1) {
     setTime(msg.startGameTime, msg.countDown);
   }
@@ -69,10 +79,10 @@ export function sendData(socketName, msg) {
 export function send() {
   //sockets send
   if (client == 0) {
-    socket.emit('playerSync', playerArray.players);
-    socket.emit('projectileSync', bulletArray.bullets);
-    socket.emit('platformSync', platformArray.plat);
-    socket.emit('bossSync', boss);
-    socket.emit('gameSync', { countDown, startGameTime });
+    socket.emit("playerSync", playerArray.players);
+    socket.emit("projectileSync", bulletArray.bullets);
+    socket.emit("platformSync", platformArray.plat);
+    socket.emit("bossSync", boss);
+    socket.emit("gameSync", { countDown, startGameTime });
   }
 }
